77eb0599ece81e076eb498d26dce58f5f60dc67a
alserembani <alserembani94@gmail.com>
2022-04-07 01:40:03 +0800

    Init dev infaqyide admin docs

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  // github: 'https://github.com/shuding/nextra',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  titleSuffix: ' – infaqYIDE',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">infaqYIDE</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Laman Dokumentasi Pembangun
       </span>
     </>
   ),
@@ -61,7 +61,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditLink: '',
+  footerText: <>{new Date().getFullYear()} © infaqYIDE.</>,
   unstable_faviconGlyph: '👋',
 }
